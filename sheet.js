393827395c317b769c8f12a092eb922fbc90a04f
Juri Di salvo <Juridisalvo11@gmail.com>
2020-05-07 00:07:13 +0200

    Google - finito

--- sheet.js
+++ sheet.js
@@ -3,11 +3,21 @@
 //     $('.dropbox').toggleClass('active');
 // })
 
-
+//Intercetto il click sull'icona del dropbox
 $('.dropbox-icon').click(function() {
+    //Controllo se ha la classe active
     if ($('.dropbox').hasClass('active')) {
+        //Se ha la classe active la tolgo
         $('.dropbox').removeClass('active');
     } else {
+        //Se non ha la classe active la metto
         $('.dropbox').addClass('active');
     }
+
+})
+
+//Intercetto il click sulla classe hide-popup
+$('.hide-popup').click(function() {
+    //Inerisco effetto fadeout per scomparire il popup
+    $('.privacy-box').fadeOut()
 })
